0fee8481ac39c49f8667571aa3ea458fbe23e99e
dedencreatifdeveloper <dedencreatifdeveloper@gmail.com>
2024-11-23 08:43:41 +0000

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,106 +1,59 @@
 <!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Starter</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="/style/plugins/fontawesome-free/css/all.min.css">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="/style/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="/style/dist/css/adminlte.min.css">
 
-    <style>
-        body{
-            /* font-family: Verdana, Geneva, Tahoma, sans-serif;
-            font-size: 11px; */
-        }
-    </style>
-    @stack('css')
+    <title>{{ $title ?? 'Page Title' }}</title>
 </head>
 
-<body class="text-sm hold-transition light-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+<body class="text-sm hold-transition dark-light sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
     <div class="wrapper">
-
-        <!-- Preloader -->
-        <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__wobble" src="/style/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60"
-                width="60">
-        </div>
-
-        <!-- Navbar -->
-        @include('layouts.navbar')
-        <!-- /.navbar -->
-
-        <!-- Main Sidebar Container -->
-        @include('layouts.sidebar')
-
-        <!-- Content Wrapper. Contains page content -->
+        <x-navbar-menu />
+        <x-sidebar-menu />
         <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
             <div class="content-header">
                 <div class="container-fluid">
                     <div class="mb-2 row">
                         <div class="col-sm-6">
-                            <h1 class="m-0">{{ $title ?? 'Title' }}</h1>
-                        </div>
+                            <h1 class="m-0">Dashboard v2</h1>
+                        </div><!-- /.col -->
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
                                 <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active">{{ $title ?? 'Title' }}</li>
+                                <li class="breadcrumb-item active">Dashboard v2</li>
                             </ol>
-                        </div>
-                    </div>
-                </div>
+                        </div><!-- /.col -->
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
             </div>
 
-            <div class="content">
+            <section class="content">
                 <div class="container-fluid">
-                    @yield('konten')
+                    <!-- Info boxes -->
+                    {{ $slot }}
                 </div>
-            </div>
+            </section>
         </div>
-
-        <aside class="control-sidebar control-sidebar-dark">
-            <div class="p-3">
-                <h5>Title</h5>
-                <p>Sidebar content</p>
-            </div>
-        </aside>
-        <!-- /.control-sidebar -->
-
-        <!-- Main Footer -->
-        <footer class="main-footer">
-            <!-- To the right -->
-            <div class="float-right d-none d-sm-inline">
-                Anything you want
-            </div>
-            <!-- Default to the left -->
-            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights
-            reserved.
-        </footer>
     </div>
-    <!-- ./wrapper -->
 
     <!-- REQUIRED SCRIPTS -->
-
     <!-- jQuery -->
     <script src="/style/plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
+    <!-- Bootstrap -->
     <script src="/style/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- overlayScrollbars -->
+    <script src="/style/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
     <!-- AdminLTE App -->
-    <script src="/style/dist/js/adminlte.min.js"></script>
-
-
-    @stack('js')
+    <script src="/style/dist/js/adminlte.js"></script>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,21 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProdukController;
+use App\Livewire\Dashboard\DashboardIndex;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('auth.login');
 // });
 
-Route::middleware(['auth'])
-    ->group(function () {
-        Route::controller(DashboardController::class)->group(function () {
-            Route::get('/', 'index')
-                ->name('dashboard.index');
-        });
+// Route::middleware(['auth'])
+//     ->group(function () {
+//         Route::controller(DashboardController::class)->group(function () {
+//             Route::get('/', 'index')
+//                 ->name('dashboard.index');
+//         });
 
+<<<<<<< HEAD
         // PRODUK
         Route::controller(ProdukController::class)->group(function () {
             Route::get('/produk', 'index')->name('produklist.index');
@@ -24,13 +26,31 @@ Route::middleware(['auth'])
             Route::get('/produk-destroy/{id}', 'destroy')->name('produklist.destroy');
         });
     });
+=======
+//         // PRODUK
+//         Route::controller(ProdukController::class)->group(function () {
+//             Route::get('/produk', 'index')->name('produklist.index');
+//             Route::get('/produk-tambah', 'create')->name('produklist.create');
+//             // Route::post('/produk-store', 'index')->name('produk.index');
+//         });
+//     });
+>>>>>>> d16ec63 (.)
 
-Route::middleware(['guest'])->group(function () {
-    Route::controller(AuthController::class)->group(function () {
-        Route::get('/login', 'login')
-            ->name('login');
+// Route::middleware(['guest'])->group(function () {
+//     Route::controller(AuthController::class)->group(function () {
+//         Route::get('/login', 'login')
+//             ->name('login');
 
+<<<<<<< HEAD
         Route::post('/login-auth', 'authenticate')
             ->name('login.auth');
     });
 });
+=======
+//         Route::post('/login-auth', 'authenticate')
+//             ->name('login.auth');
+//     });
+// });
+
+Route::get('/',DashboardIndex::class)->name('dashboard.index');
+>>>>>>> d16ec63 (.)
